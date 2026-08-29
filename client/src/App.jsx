a1dea24c95296563dc994a9fea384b6640a1e00f
soumya chattopadhyay <151312846+knightmare10@users.noreply.github.com>
2026-08-29 22:23:29 +0530

    Add pages and update Job Portal frontend

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,9 +1,11 @@
 import React from 'react'
+import { Route, Routes } from 'react-router-dom'
+import Home from './pages/Home'
 
 const App = () => {
   return (
     <div>
-      
+      <Route path='/' element={<Home/>} />
     </div>
   )
 }
