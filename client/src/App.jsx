44e27e76ec572f5d3f8aff373eb4c058ddb1bd64
soumya chattopadhyay <151312846+knightmare10@users.noreply.github.com>
2026-08-30 17:49:38 +0530

    modified the app.jsx page

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,11 +1,17 @@
 import React from 'react'
 import { Route, Routes } from 'react-router-dom'
 import Home from './pages/Home'
+import Applyjob from './pages/Applyjob'
+import Application from './pages/Application'
 
 const App = () => {
   return (
     <div>
-      <Route path='/' element={<Home/>} />
+      <Routes>
+        <Route path='/' element={<Home />} />
+        <Route path='/apply-job/:id' element={<Applyjob />} />
+        <Route path='/applications' element={<Application />} />
+      </Routes>
     </div>
   )
 }
--- client/src/pages/Applyjob.jsx
+++ client/src/pages/Applyjob.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 const Applyjob = () => {
   return (
     <div>
-      
+      applyjob
     </div>
   )
 }
