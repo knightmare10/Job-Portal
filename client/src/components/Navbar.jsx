ffe82766f30e091f909aa91f59c19c0c461c898a
soumya chattopadhyay <151312846+knightmare10@users.noreply.github.com>
2026-08-31 18:46:55 +0530

    modified the navbar and create the Hero.jsx file

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -16,18 +16,18 @@ const Navbar = () => {
 
                 <div className='flex gap-4 max-sm:text-xs items-center'>
 
-                    {user ? 
-                        <div className='flex items-center gap-4'>
-                            <Link to='/applications'>Applied Jobs</Link>
-                            <p>|</p>
-                            <p>Hi, {user.firstName} {user.lastName}</p>
-
-                            <UserButton />
-                        </div>
-                     :<div>
-                            <button className='text-gray-600'>Recruiter Login</button>
-                            <button onClick={() => openSignIn()}className='bg-blue-600 text-white px-6 sm:px-9 py-2 rounded-full'>Login</button>
-                        </div>
+                    {
+                        user
+                        ? <div className='flex items-center gap-3'>
+                                <Link to='/applications'>Applied Jobs</Link>
+                                <p>|</p>
+                                <p>Hi, {user.firstName} {user.lastName}</p>
+                                <UserButton />
+                            </div>
+                            : <div>
+                                <button className='text-gray-600'>Recruiter Login</button>
+                                <button onClick={() => openSignIn()} className='bg-blue-600 text-white px-6 sm:px-9 py-2 rounded-full'>Login</button>
+                            </div>
                     }
 
                 </div>
