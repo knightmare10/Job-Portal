ad30553a32218f3e6e777d0c53973dfee0c74717
soumya chattopadhyay <151312846+knightmare10@users.noreply.github.com>
2026-09-02 23:46:56 +0530

    modified jobListing,navbar,appcontext,index.css,home.jsx and created appdownload and jobcard

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -2,13 +2,15 @@ import React from 'react'
 import Navbar from '../components/Navbar'
 import Hero from '../components/Hero'
 import JobListing from '../components/JobListing'
+import AppDownload from '../components/AppDownload'
 
 const Home = () => {
   return (
     <div>
       <Navbar />
-      <Hero/>
-      <JobListing/>
+      <Hero />
+      <JobListing />
+      <AppDownload />
     </div>
   )
 }
