f86d85afb30aa8a1afd0d734c7698b22e4b0bd25
soumya chattopadhyay <151312846+knightmare10@users.noreply.github.com>
2026-09-03 23:46:22 +0530

    created footer.jsx and modified jobcard,joblisting,applyjob and home.jsx

--- client/src/pages/Applyjob.jsx
+++ client/src/pages/Applyjob.jsx
@@ -1,9 +1,30 @@
-import React from 'react'
+import React, { useContext, useEffect, useState } from 'react'
+import { useParams } from 'react-router-dom'
+import { AppContext } from '../context/AppContext'
 
 const Applyjob = () => {
+
+  const { id } = useParams()
+
+  const [JobData,setJobData] = useState(null)
+
+  const { jobs } = useContext(AppContext)
+
+  const fetchJob = async () =>{
+    const data = jobs.filter(job => job._id === id)
+    if(data.length !== 0){
+      setJobData(data[0])
+      console.log(data[0]);
+    }
+  }
+
+  useEffect(()=>{
+    fetchJob()
+  },[id,jobs])
+
   return (
     <div>
-      applyjob
+      
     </div>
   )
 }
