e4471b515194558f655e5f265f5eae4e774778b9
soumya chattopadhyay <151312846+knightmare10@users.noreply.github.com>
2026-09-07 00:39:22 +0530

    modified app,navbar, appcontext and create and update the recruiterlogin.jsx

--- client/src/context/AppContext.jsx
+++ client/src/context/AppContext.jsx
@@ -14,6 +14,8 @@ export const AppContextProvider = (props) =>{
 
     const [jobs, setJobs] = useState([])
 
+    const [showRecruiterLogin, setShowRecruiterLogin] = useState(false)
+
     // Functions to fetch jobs
     const fetchJobs = async ()=>{
         setJobs(jobsData)
@@ -26,7 +28,8 @@ export const AppContextProvider = (props) =>{
     const value = {
         setSearchFilter,searchFilter,
         isSearched,setIsSearched,
-        jobs, setJobs
+        jobs, setJobs,
+        showRecruiterLogin,setShowRecruiterLogin,
     }
 
     return (<AppContext.Provider value={value}>
