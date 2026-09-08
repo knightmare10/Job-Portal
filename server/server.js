4cf2a6c32b3d05a2e8874fa069ec4d584ed83fda
soumya chattopadhyay <151312846+knightmare10@users.noreply.github.com>
2026-09-08 23:26:03 +0530

    add the server folder

---
+++ server/server.js
@@ -0,0 +1,22 @@
+import express from 'express'
+import cors from 'cors'
+import 'dotenv/config'
+
+
+// Initialize Express
+const app = express()
+
+// Middlewares
+app.use(cors())
+app.use(express.json())
+
+// Routes
+app.get('/',(req,res)=>res.send("API working"))
+
+// Port
+const PORT = process.env.PORT || 5000
+
+app.listen(PORT,()=>{
+    console.log(`server is running on port ${PORT}`);
+    
+})
