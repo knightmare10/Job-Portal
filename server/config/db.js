ef892278379a1215680f7c3191660ba6859c2a90
soumya chattopadhyay <151312846+knightmare10@users.noreply.github.com>
2026-09-09 22:12:08 +0530

    update the server folder

---
+++ server/config/db.js
@@ -0,0 +1,12 @@
+import mongoose  from "mongoose";
+
+//Function to connect to the Mongodb database
+const connectDB = async ()=> {
+
+    mongoose.connection.on('connected', () =>console.log('Database Connected'))
+
+    await mongoose.connect(`${process.env.MONGODB_URI}/job-portal`)
+
+}
+
+export default connectDB
---
+++ server/config/instrument.js
@@ -0,0 +1,28 @@
+// Import with `import * as Sentry from "@sentry/node"` if you are using ESM
+import * as Sentry from "@sentry/node"
+import { nodeProfilingIntegration } from "@sentry/profiling-node";
+
+Sentry.init({
+  dsn: "https://985334577ff7d970f5120a40e9448429@o4512056382324736.ingest.us.sentry.io/4512056393793536",
+  integrations: [
+    nodeProfilingIntegration(),
+    Sentry.mongooseIntegration()
+  ],
+  // Tracing
+//   tracesSampleRate: 1.0, // Capture 100% of the transactions
+});
+
+// Manually call startProfiler and stopProfiler
+// to profile the code in between
+Sentry.profiler.startProfiler();
+
+// Starts a transaction that will also be profiled
+Sentry.startSpan({
+  name: "My First Transaction",
+}, () => {
+  // the code executing inside the transaction will be wrapped in a span and profiled
+});
+
+// Calls to stopProfiling are optional - if you don't stop the profiler, it will keep profiling
+// your application until the process exits or stopProfiling is called.
+Sentry.profiler.stopProfiler();
