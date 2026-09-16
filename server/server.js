a7fb0a73ad386bad4de49e470ac24a855a32289b
soumya chattopadhyay <151312846+knightmare10@users.noreply.github.com>
2026-09-16 23:27:01 +0530

    modified server.js, update and create userController,JobApplications and userRoutes

--- server/server.js
+++ server/server.js
@@ -8,6 +8,8 @@ import { clerkWebhooks } from './controllers/webhooks.js'
 import companyRoutes  from './routes/companyRoutes.js'
 import connectCloudinary from './config/cloudinary.js'
 import jobRoutes from './routes/jobRoutes.js'
+import userRoutes from './routes/userRoutes.js'
+import {clerkMiddleware} from '@clerk/express'
 
 
 // Initialize Express
@@ -20,6 +22,7 @@ await connectCloudinary()
 // Middlewares
 app.use(cors())
 app.use(express.json())
+app.use(clerkMiddleware())
 
 // Routes
 app.get('/',(req,res)=>res.send("API working"))
@@ -29,6 +32,7 @@ app.get("/debug-sentry", function mainHandler(req, res) {
 app.post('/webhooks',clerkWebhooks)
 app.use('/api/company', companyRoutes)
 app.use('/api/jobs',jobRoutes)
+app.use('/api/users', userRoutes)
 
 // Port
 const PORT = process.env.PORT || 5000
