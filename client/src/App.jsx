6961720047f9c2bd0fc334c3e044c3f1b4de786d
soumya chattopadhyay <151312846+knightmare10@users.noreply.github.com>
2026-09-18 23:22:52 +0530

    modified the app.jsx, recruiterlogin,appcontext,dashboard.jsx and in the server folder companycontroller also modified

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -10,21 +10,29 @@ import AddJob from './pages/AddJob'
 import Managejobs from './pages/Managejobs'
 import ViewApplications from './pages/ViewApplications'
 import 'quill/dist/quill.snow.css'
+import { ToastContainer, toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 
 const App = () => {
 
-  const {showRecruiterLogin} = useContext(AppContext)
+  const { showRecruiterLogin, companyToken } = useContext(AppContext)
   return (
     <div>
-      { showRecruiterLogin && <RecruiterLogin />}
+      {showRecruiterLogin && <RecruiterLogin />}
+      <ToastContainer />
       <Routes>
         <Route path='/' element={<Home />} />
         <Route path='/apply-job/:id' element={<Applyjob />} />
         <Route path='/applications' element={<Application />} />
-        <Route path='/dashboard' element={<Dashboard/>}>
-          <Route path='add-job' element={<AddJob/>}/>
-          <Route path='manage-jobs' element={<Managejobs/>}/>
-          <Route path='view-applications' element={<ViewApplications/>}/>
+        <Route path='/dashboard' element={<Dashboard />}>
+          {
+            companyToken ? <>
+              <Route path='add-job' element={<AddJob />} />
+              <Route path='manage-jobs' element={<Managejobs />} />
+              <Route path='view-applications' element={<ViewApplications />} />
+            </> : null
+          }
+
         </Route>
       </Routes>
     </div>
